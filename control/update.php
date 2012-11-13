edbf59fa95d244f44def6a3856df27efdce4b58e
Paolo Ciarrocca <paolo.ciarrocca@gmail.com>
2012-11-13 21:01:57 +0000

    comforming to pull request guideline

--- control/update.php
+++ control/update.php
@@ -1,15 +1,15 @@
 <?php
 if(!empty($_POST['url'])) {
-	$filename = '../url';
-	$pattern = "/(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/";
-	if(is_writable($filename) && preg_match($pattern, $_POST['url']) == 1) {
-		$h = fopen($filename, 'w');
-		fwrite($h, $_POST['url']);
-		fclose($h);
-		echo "Success! Go check your devices.";
-		exit;
-	} else {
-    	echo "Error: The URL file is not writable.";
-	}
+  $filename = '../url';
+  $pattern = "/(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/";
+  if(is_writable($filename) && preg_match($pattern, $_POST['url']) == 1) {
+    $h = fopen($filename, 'w');
+    fwrite($h, $_POST['url']);
+    fclose($h);
+    echo "Success! Go check your devices.";
+    exit;
+  } else {
+    echo "Error: The URL file is not writable.";
+  }
 }
 ?>
